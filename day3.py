c0d961872092a3f0e8176cf2819b03235e43b820
Andrew Norman <andy@norman.cx>
2019-12-03 07:33:34 +0000

    Day 3.2

--- day3.py
+++ day3.py
@@ -5,6 +5,7 @@ import sys
 class Wire():
     def __init__(self, moves):
         self.cells_passed = set()
+        self.cell_min_steps = {}
         self.moves = moves
         self.calc_path()
 
@@ -18,12 +19,16 @@ class Wire():
 
         x = 0
         y = 0
+        steps = 0
 
         for (direction, distance) in self.moves:
             for pos in range(0, distance):
+                steps += 1
                 x += vectors[direction][0]
                 y += vectors[direction][1]
                 self.cells_passed.add((x, y))
+                if not((x, y) in self.cell_min_steps):
+                    self.cell_min_steps[(x, y)] = steps
 
 def solve1(input):
 
@@ -40,15 +45,17 @@ def solve1(input):
         distance  = calc_dist(crossing, (0, 0))
         if distance < min_distance:
             min_distance = distance
-    
-    return (min_distance, crossings)
 
-def solve2(input):
-    return
+    min_distance_2 = 1000000000
+    for crossing in crossings:
+        distance  = wire_one.cell_min_steps[crossing] + wire_two.cell_min_steps[crossing]
+        if distance < min_distance_2:
+            min_distance_2 = distance
+    
+    return (min_distance, min_distance_2, crossings)
 
 def calc_dist(one, two):
     return abs(one[0] - two[0]) + abs(one[1] - two [1])
-
 
 def convert(lines):
     return [
@@ -70,19 +77,12 @@ def convert_line(line):
     return moves
 
 data = IH.InputHelper(3).readlines()
-print(data)
 
 print('Part 1 ', solve1(data))
 print('6 3,3', solve1(['R8,U5,L5,D3', 'U7,R6,D4,L4']))
 
-print('159', solve1(['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']))
+print('159 610', solve1(['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']))
 
-print('135', solve1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']))
+print('135 410', solve1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']))
 
 
-#print('2,0,0,0,99 ', solve1('1,0,0,0,99'))
-#print('2,3,0,6,99 ', solve1('2,3,0,3,99'))
-#print('2,4,4,5,99,9801 ', solve1('2,4,4,5,99,0'))
-#print('1,1,1,4,99,5,6,0,99 ', solve1('1,1,1,4,99,5,6,0,99'))
-
-#print('Part 2 ', solve2(data[0]))
