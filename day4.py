c44d678ecaee3d7ecbd8943352432a5c0be4332f
Andrew Norman <andy@norman.cx>
2019-12-04 07:17:50 +0000

    Day 4 - tidying

--- day4.py
+++ day4.py
@@ -62,7 +62,7 @@ def is_valid_2(value):
     return two_the_same
 
 
-#print('Part 1 ', solve1((264793, 803935)))
+print('Part 1 ', solve1((264793, 803935)))
 
 print('True 111111', is_valid(111111))
 print('False 223450', is_valid(223450))
@@ -74,14 +74,3 @@ print('False 123444', is_valid_2(123444))
 print('True 111122', is_valid_2(111122))
 
 print('Part 2 ', solve2((264793, 803935)))
-
-
-#print('6 3,3', solve1(['R8,U5,L5,D3', 'U7,R6,D4,L4']))
-
-
-
-#print('159 610', solve1(['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']))
-
-#print('135 410', solve1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']))
-
-
